e20392ac312ad2a31c9147416178ac7a4f82ef3d
Vedant Kherade <152008775+vedantkherade@users.noreply.github.com>
2026-08-06 14:24:32 +0530

    Time: 2 ms (73.37%), Space: 56.8 MB (60.44%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let candidate = null;
+    let count = 0;
+
+    for(let num of nums){
+        if(count == 0){
+            candidate = num;
+        }
+        if(num == candidate){
+            count++;
+        }else{
+            count--;
+        }
+    }
+    return candidate;
+};
